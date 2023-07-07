e58bdc0e67cee14a252fc8feb59fa765e8e9f95c
adrianusyoga1p <adrianusyoga26@gmail.com>
2023-07-07 11:42:21 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+window.onload = () => {
+    "use strict"
+    const sections = document.querySelectorAll('section');
+    const btnToggle = document.querySelector('.btn-toggle');
+    const navbar = document.querySelector('.navbar');
+    
+    window.addEventListener('scroll', navHighlighter);
+    function navHighlighter(){
+        let scrollY = window.pageYOffset;
+        sections.forEach(current => {
+            const sectionHeight = current.offsetHeight;
+            const sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute("id");
+
+            if (
+                scrollY > sectionTop &&
+                scrollY <= sectionTop + sectionHeight
+              ){
+                document.querySelector(".navbar-link[href*=" + sectionId + "]").classList.add("active");
+                navbar.classList.remove('show');
+                btnToggle.classList.remove('toggled');
+              } else {
+                document.querySelector(".navbar-link[href*=" + sectionId + "]").classList.remove("active");
+              }
+        });
+    }
+
+    // for(let i = 0; i < btnToggle.length; i++){
+        btnToggle.onclick = (e) => {
+            e.preventDefault();
+            navbar.classList.toggle('show');
+            btnToggle.classList.toggle('toggled');
+        }
+    // }
+    
+}
